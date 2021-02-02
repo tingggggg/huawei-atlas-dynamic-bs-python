b443408208609a520a9d38d7b485d4886f5f0d47
Tim.Lee <shinting840@gmail.com>
2021-02-02 18:26:12 +0800

    add comment

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # huawei-atlas-dynamic-bs-python
+ref: 
+/usr/local/Ascend/ascend-toolkit/20.1.rc1/pyACL/sample/acl_execute_model/acl_resnet_sync/
+(C++) https://gitee.com/ascend/samples/tree/master/cplusplus/level2_simple_inference/1_classification/googlenet_imagenet_dynamic_batch
--- acl_infer.py
+++ acl_infer.py
@@ -67,7 +67,7 @@ class Net(object):
         self.load_input_dataset = None
         self.load_output_dataset = None
 
-        self.img_buffer_size = input_size[0] * input_size[1] * input_size[2] // 2
+        self.img_buffer_size = (input_size[0] * input_size[1] * input_size[2]) // 2
         self.batch_size = 1
         self.number_iterations = 3
 
@@ -151,21 +151,50 @@ class Net(object):
                 self.output_data.append({"buffer": temp_buffer,
                                          "size": temp_buffer_size})
 
-    def _data_interaction(self, dataset, policy=ACL_MEMCPY_HOST_TO_DEVICE):
-        """Copy data from host to device or from device to host that according "policy"
+    def data_interaction(self, dataset, policy=ACL_MEMCPY_HOST_TO_DEVICE):
+        """Copy data from host to device or from device to host  that according "policy"
         Copy data from host to device:
             dynamic batch size model need to apply 2 input buffer 
 
-            for i in range(input_num: 2):
+            for i in range(int input_num: 2):
                 input_name = acl.mdl.get_input_name_by_index(model_desc, i)
-                print(input_name)
-            
-            out:
+                print(input_name)         
+            out->
                 X
                 ascend_mbatch_shape_data
-            just copy pointer of img to (input_name=="X") buffer
-            Only 1 input buffer (input_name=="X") if your model is static batch size
 
+            just copy pointer of img to (input_name=="X") buffer
+            Only 1 input buffer (input_name: "X") if your model is static batch size
+
+
+            image need to be copied to device one by one, 
+            according to (idx * self.img_buffer_size) to move your buffer pointer to stack your images data
+
+            for idx, image in enumerate(images):
+                ptr = acl.util.numpy_to_ptr(image)
+                ret = acl.rt.memcpy(item["buffer"] + (idx * self.img_buffer_size),
+                                    item["size"],
+                                    ptr,
+                                    self.img_buffer_size,
+                                    policy)
+            
+            ret = acl.rt.memcpy(dst, dest_max, src, count, kind)
+            dst(int)： des pointer address
+            dest_max(int)： maximum size of des (Byte)
+            src(int)： host pointer address
+            count(int)： lenght of host data (Byte)
+
+        Copy data from device to host:
+            only 1 output if model is classification
+            need to malloc host memory first for store output result
+
+
+        Args:
+            dataset(list(array)): image list
+            policy(int): from host or from device
+        
+        Returns:
+            None
         """
         temp_data_buffer = self.input_data \
             if policy == ACL_MEMCPY_HOST_TO_DEVICE \
@@ -199,6 +228,11 @@ class Net(object):
                 check_ret("acl.rt.memcpy", ret)
 
     def _gen_dataset(self, type_str="input"):
+        """According buffer to create dataset(acl.mdl.create_dataset()) object for execute 
+
+        Args:
+            type_str(str): For choose to create  which dataset(input or output)
+        """
         dataset = acl.mdl.create_dataset()
 
         temp_dataset = None
@@ -223,33 +257,46 @@ class Net(object):
 
     def _data_from_host_to_device(self, images):
         # copy images to device
-        self._data_interaction(images, ACL_MEMCPY_HOST_TO_DEVICE)
+        self.data_interaction(images, ACL_MEMCPY_HOST_TO_DEVICE)
         # load input data into model
         self._gen_dataset("in")
         # load output data into model
         self._gen_dataset("out")
 
     def _data_from_device_to_host(self):
+        """Copy model output from device to host
+        You have not do call this function if you don't need print out predict results
+        """
         res = []
         # copy device to host
-        self._data_interaction(res, ACL_MEMCPY_DEVICE_TO_HOST)
+        self.data_interaction(res, ACL_MEMCPY_DEVICE_TO_HOST)
         result = self.get_result(res)
-        # self._print_result(result)
+        self._print_result(result)
 
     def run(self, images, batch_size=1, number_iter=3):
         """ Copy host data to devcie then do inference, finally copy device data to host
         Args:
             images(numpy.array): A flatten image array e.g. images.shape(150528, )
+            number_iter(int): Total number of times for each execute
         Returns:
             res_time(float): from forward() (ms)
         """
         self.batch_size = batch_size
         self._data_from_host_to_device(images)
         res_time = self.forward(number_iter)
-        self._data_from_device_to_host()
+        # self._data_from_device_to_host()
         return res_time
 
     def forward(self, number_iter=3):
+        """Set batch szie first by acl.mdl.set_dynamic_batch_size()
+        then do infer by acl.mdl.execute()
+
+        Args:
+            number_iter(int): Total number of times for each execute
+        
+        Returns: 
+            res_time(float): Average times of execute (ms)
+        """
         i, ret = acl.mdl.get_input_index_by_name(self.model_desc, dynamicFlag)
         check_ret("acl.mdl.get_input_index_by_name", ret)
         ret = acl.mdl.set_dynamic_batch_size(self.model_id, self.load_input_dataset, i, self.batch_size)
@@ -270,10 +317,27 @@ class Net(object):
         return res_time
 
     def _print_result(self, result):
+        MODEL_MAXIMUM_BS = 128
+        """Print out predict result:
+        e.g. 
+        ======== top5 inference results: =============
+        Idx -> 0
+        [640]: 0.656250
+        [678]: 0.046082
+        [825]: 0.035309
+        [199]: 0.022812
+        [493]: 0.022812
+
+        Args:
+            result(numpy array): array format fo results
+        
+        Returns:
+            None
+        """
         tuple_st = struct.unpack("{:}f".format(str(result[0].shape[0] // 4)), bytearray(result[0]))
         # print(tuple_st)
         vals = np.array(tuple_st).flatten()
-        vals = vals.reshape(8, -1)
+        vals = vals.reshape(MODEL_MAXIMUM_BS, -1)
         for b in range(self.batch_size):
             top_k = vals[b].argsort()[-1:-6:-1]
             print("======== top5 inference results: =============")
@@ -282,6 +346,8 @@ class Net(object):
                 print("[%d]: %f" % (j, vals[b][j]))
 
     def _destroy_databuffer(self):
+        """Release data buffer by mormal way
+        """
         for dataset in [self.load_input_dataset, self.load_output_dataset]:
             if not dataset:
                 continue
@@ -295,6 +361,13 @@ class Net(object):
             check_ret("acl.mdl.destroy_dataset", ret)
 
     def get_result(self, output_data):
+        """Convert the already copied to host buffer result to numpy format
+        Args:
+            output_data(host buffer): results(in host buffer)
+
+        Returns:
+            numpy array results
+        """
         dataset = []
         for temp in output_data:
             size = temp["size"]
